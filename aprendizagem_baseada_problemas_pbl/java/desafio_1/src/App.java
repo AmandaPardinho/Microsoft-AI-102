fbf4c15ecdce1ad3d29d06b683867070b8099bd8
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2024-10-31 21:09:15 -0300

    refatoração do código

--- aprendizagem_baseada_problemas_pbl/java/desafio_1/src/App.java
+++ aprendizagem_baseada_problemas_pbl/java/desafio_1/src/App.java
@@ -11,22 +11,17 @@ public class App {
         System.out.println("Digite o valor do benefício: ");
         float beneficio = scanner.nextFloat();
         
-        float imposto = 0f;
-        float salarioTotal = (salario - imposto) + beneficio;
-
+        float imposto = 0f;       
         if(salario > 0 && salario <= 1100.00f){
             imposto = 0.05f * salario;
-            salarioTotal = (salario - imposto) + beneficio;
-            System.out.println("Salário total: " + salarioTotal);
         }else if(salario > 1100.00f && salario <= 2500.00f){
-            imposto = 0.10f * salario;
-            salarioTotal = (salario - imposto) + beneficio;
-            System.out.println("Salário total: " + salarioTotal);
+            imposto = 0.10f * salario;            
         }else{
-            imposto = 0.15f * salario;
-            salarioTotal = (salario - imposto) + beneficio;
-            System.out.println("Salário a receber: " + String.format("%.2f",salarioTotal));
+            imposto = 0.15f * salario;        
         }
+        
+        float salarioTotal = (salario - imposto) + beneficio;
+        System.out.println("Salário a receber: " + String.format("%.2f",salarioTotal));
 
         scanner.close();
     }
