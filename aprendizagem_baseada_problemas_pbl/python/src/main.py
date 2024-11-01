a78238c35f148eefd981342e27a3b0909073e639
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2024-11-01 00:19:07 -0300

    retirada do ponto e vírgula

--- aprendizagem_baseada_problemas_pbl/python/src/main.py
+++ aprendizagem_baseada_problemas_pbl/python/src/main.py
@@ -1,22 +1,22 @@
-salario_string = input("Digite o valor do salário: ");
-salario = float(salario_string);
+salario_string = input("Digite o valor do salário: ")
+salario = float(salario_string)
 
-beneficios_string = input("Digite o valor dos benefícios: ");
-beneficios = float(beneficios_string);
+beneficios_string = input("Digite o valor dos benefícios: ")
+beneficios = float(beneficios_string)
 
 def calcularImposto(salario):
     aliquota = 0;
     if salario > 0 and salario <= 1100.00:
-        aliquota = 0.05;
+        aliquota = 0.05
     elif salario >= 1100 and salario < 2500:
-        aliquota = 0.1;
+        aliquota = 0.1
     else:
-        aliquota = 0.15;    
-    return aliquota * salario; 
+        aliquota = 0.15    
+    return aliquota * salario 
 
-imposto = calcularImposto(salario);
-salarioLiquido = (salario - imposto) + beneficios;
+imposto = calcularImposto(salario)
+salarioLiquido = (salario - imposto) + beneficios
 
 #dois possíveis jeitos de formatar a saída
-#print("O valor do salário líquido é de ${}".format(salarioLiquido));
-print(f'O valor do salário líquido é de $ {salarioLiquido:.2f}');
+#print("O valor do salário líquido é de ${}".format(salarioLiquido))
+print(f'O valor do salário líquido é de $ {salarioLiquido:.2f}')
