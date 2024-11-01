70f6cd4df145bc71110dd79523e270a4aac13d4f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2024-11-01 15:11:50 -0300

    atualização cache c#

--- aprendizagem_baseada_problemas_pbl/csharp/Desafio1/obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
+++ aprendizagem_baseada_problemas_pbl/csharp/Desafio1/obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0c622ef38744e5d1567cdab54e883f97962c19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78238c35f148eefd981342e27a3b0909073e639")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
