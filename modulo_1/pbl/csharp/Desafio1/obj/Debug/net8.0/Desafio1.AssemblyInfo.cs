27d85c028aafa6530d7bf2aaa3a88c065367c8ad
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2024-11-24 17:17:13 -0300

    -refatoração de pasta -anotações do módulo 3

--- modulo_1/pbl/csharp/Desafio1/obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
+++ modulo_1/pbl/csharp/Desafio1/obj/Debug/net8.0/Desafio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dfb09fe6c4025f663fdf4eeab7244ce4e52c3ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae29a190a926bf7dc004abf89fc29a790e0187d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
